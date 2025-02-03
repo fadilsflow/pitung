a1f45799ac5006001e3de190aff1185ec0b33043
WAHYUAKHMADFADILLAH <wahyufadil1140@gmail.com>
2025-02-04 00:44:19 +0700

    cleanup

--- src/components/button/LogoutButton.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { Button } from "../ui/button";
-
-import { logout } from "./action";
-
-export default function LogoutButton() {
-  return <Button onClick={() => logout()}>Logout</Button>;
-}
--- src/components/landiing/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="flex items-center justify-center mt-0">
-    <p className="text-center text-sm text-muted-foreground">
-      © {new Date().getFullYear()} Pitung. All rights reserved.
-    </p>
-  </footer>
-  );
-}
